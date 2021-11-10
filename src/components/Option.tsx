a01455d37fbade2fd4444f473418a96038bb140d
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-10 21:39:11 +0530

    add: added query setter store for the axios

--- src/components/Option.tsx
+++ src/components/Option.tsx
@@ -1,8 +1,35 @@
 import React from "react";
 import styles from "../styles/Options.module.scss";
+import MenuContainer from "./utils/Menu";
 
 const Options: React.FC = () => {
-  return <div className={styles.OptionContainer}></div>;
+  const [currentQuery, setCurrentQuery] = React.useState<
+    {
+      name: string;
+      value: any;
+    }[]
+  >([{}] as any);
+
+  const updateQueryHandler = (optionName: string, value: any) => {
+    setCurrentQuery((prev) =>
+      !prev.includes({ name: optionName, value })
+        ? [...prev, { name: optionName, value }]
+        : prev
+    );
+  };
+
+  return (
+    <div className={styles.OptionContainer}>
+      <h1>Filter your preferences</h1>
+      <section>
+        <MenuContainer
+          items={["WB", "AP", "GJ", "RJ", "JK", "SK", "NG", "KL"]}
+          name="location"
+          clickHandler={updateQueryHandler}
+        />
+      </section>
+    </div>
+  );
 };
 
 export default Options;
--- src/components/utils/Menu.tsx
+++ src/components/utils/Menu.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Dropdown, Menu } from "antd";
 
-const Menu: React.FC = () => {
-  return <></>;
+interface MenuProps {
+  items: string[];
+  clickHandler: (optionName: string, value: any) => void;
+  name: string;
+}
+
+const MenuContainer: React.FC<MenuProps> = ({ items, clickHandler, name }) => {
+  return (
+    <Dropdown
+      overlay={
+        <Menu>
+          {items.map((item, index) => (
+            <Menu.Item key={index} onClick={() => clickHandler(item, name)}>
+              {item}
+            </Menu.Item>
+          ))}
+        </Menu>
+      }
+    >
+      {name}
+    </Dropdown>
+  );
 };
 
-export default Menu;
+export default MenuContainer;
