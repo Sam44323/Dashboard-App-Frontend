1599328a8edbcf0774aca610d2a3dff8bfdfc711
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-10 20:11:07 +0530

    add: added route for container for college

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import Header from "./components/Header";
 import { Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
+import CollegeContainer from "./components/CollegeContainer";
 
 const Router: React.FC = () => {
   return (
     <>
       <Header />
       <Switch>
-        <Route exact component={Home} />
+        <Route exact path="/" component={Home} />
+        <Route path="/college" component={CollegeContainer} />
       </Switch>
     </>
   );
