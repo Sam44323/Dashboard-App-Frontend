a24248b8ea30cab56b2ed48474397915145cb3b0
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-10 20:10:20 +0530

    add: added component for container for college

---
+++ src/components/CollegeContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/CollegeContainer.module.scss";
+
+const CollegeContainer: React.FC = () => {
+  return <></>;
+};
+
+export default CollegeContainer;
