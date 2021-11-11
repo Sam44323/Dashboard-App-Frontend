e03ae291b9bb932f82f27e458e4bcfc7bd67e2d2
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-11 23:51:40 +0530

    add: added route for details for college

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import { Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import CollegeContainer from "./components/CollegeContainer";
+import CollegeDetails from "./components/CollegeDetails";
 
 const Router: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const Router: React.FC = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/colleges" component={CollegeContainer} />
+        <Route path="/college/:id" component={CollegeDetails} />
       </Switch>
     </>
   );
