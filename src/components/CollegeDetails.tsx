32da4c36f893c6fdba7bdb4efcc223838678a257
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-11 23:51:02 +0530

    add: added component for detail view for colleges

---
+++ src/components/CollegeDetails.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/CollegeDetails.module.scss";
+
+const CollegeDetails: React.FC = () => {
+  return <></>;
+};
+
+export default CollegeDetails;
