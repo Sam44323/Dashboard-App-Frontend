28497d494dc61daef251a721fbcc100749c5f79c
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-12 22:24:32 +0530

    add: added fetcher for details for college

--- src/components/CollegeDetails.tsx
+++ src/components/CollegeDetails.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import styles from "../styles/CollegeDetails.module.scss";
 import { useParams } from "react-router";
+import Axios from "../api/axios.config";
 
 const CollegeDetails: React.FC = () => {
   const params = useParams();
+  const [collegeData, setCollegeData] = React.useState<any>();
+  React.useEffect(() => {
+    const getCollege = async () => {
+      console.log(params);
+      const response = await Axios.get(`/college?id=${(params as any).id}`);
+      console.log(response.data.colleges[0]);
+    };
+
+    getCollege();
+  }, [params]);
 
   return <></>;
 };
